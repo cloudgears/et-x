ed8cb8c595e8b7e094a0f2919bf78197a25e0ad9
Sergey Reznik <rezniksa@gmail.com>
2013-08-08 17:42:18 +0300

    table control

--- include/et-ext/gui/table.h
+++ include/et-ext/gui/table.h
@@ -18,7 +18,47 @@ namespace et
 		public:
 			ET_DECLARE_POINTER(Table)
 			
+			class Section
+			{
+			public:
+				Element2d::Pointer header;
+				Element2d::List items;
+				Element2d::Pointer footer;
+				
+			private:
+				Section();
+				friend class Table;
+				
+			private:
+				float headerOffset;
+				float footerOffset;
+				
+				float headerSize;
+				float itemsSize;
+				float footerSize;
+				
+				float sectionSize;
+			};
+			
+		public:
 			Table(et::gui::Element2d*, const std::string& name = std::string());
+			~Table();
+			
+			Section* addSection(Element2d::Pointer header, const Element2d::List& items,
+				Element2d::Pointer footer);
+			
+			void clean();
+
+		private:
+			void layout(const vec2&);
+			void autoLayout(const vec2& contextSize, float duration);
+			
+			void layoutChildren(const vec2&);
+			
+			void setOffsetDirectly(const vec2&);
+			
+		private:
+			std::vector<Section*> _sections;
 		};
 	}
 }
--- src/gui/table.cpp
+++ src/gui/table.cpp
@@ -10,10 +10,135 @@
 using namespace et;
 using namespace et::gui;
 
-ET_DECLARE_GUI_ELEMENT_CLASS(Scroll)
+ET_DECLARE_GUI_ELEMENT_CLASS(Table)
+
+Table::Section::Section() :
+	headerOffset(0.0f), footerOffset(0.0f), headerSize(0.0f), itemsSize(0.0f),
+	footerSize(0.0f), sectionSize(0.0f) { }
 
 Table::Table(et::gui::Element2d* parent, const std::string& name) :
 	Scroll(parent, ET_GUI_PASS_NAME_TO_BASE_CLASS)
 {
-	setBackgroundColor(vec4(1.0f));
+	setBounce(Bounce_Horizontal);
+	setFlag(Flag_HandlesChildLayout);
+}
+
+Table::~Table()
+{
+	clean();
+}
+
+void Table::layout(const vec2& sz)
+{
+	Scroll::layout(sz);
+	setOffsetDirectly(offset());
+	layoutChildren(size());
+}
+
+void Table::autoLayout(const vec2& contextSize, float duration)
+{
+	Scroll::autoLayout(contextSize, duration);
+	setOffsetDirectly(offset());
+	layoutChildren(size());
+}
+
+void Table::layoutChildren(const vec2& ownSize)
+{
+	float x = 0.0f;
+	
+	for (auto s : _sections)
+	{
+		if (s->header.valid())
+		{
+			s->header->setFrame(rect(x + s->headerOffset, 0.0f, s->header->size().x, ownSize.y));
+			x += s->header->frame().width;
+		}
+		
+		for (auto i : s->items)
+		{
+			i->setFrame(rect(x, 0.0f, i->size().x, ownSize.y));
+			x += i->frame().width;
+		}
+		
+		if (s->footer.valid())
+		{
+			s->footer->setFrame(rect(x + s->footerOffset, 0.0f, s->footer->size().x, ownSize.y));
+			x += s->footer->frame().width;
+		}
+	}
+
+	adjustContentSize();
+}
+
+Table::Section* Table::addSection(Element2d::Pointer header, const Element2d::List& items,
+	Element2d::Pointer footer)
+{
+	Section* section = new Section;
+	
+	if (header.valid())
+	{
+		section->header = header;
+		section->header->setParent(this);
+		section->headerSize = section->header->size().x;
+		section->sectionSize += section->headerSize;
+	}
+	
+	section->items = items;
+	for (auto i : section->items)
+	{
+		i->setParent(this);
+		section->itemsSize += i->size().x;
+	}
+	section->sectionSize += section->itemsSize;
+	
+	if (footer.valid())
+	{
+		section->footer = footer;
+		section->footer->setParent(this);
+		section->footerSize = section->footer->size().x;
+		section->sectionSize += section->footerSize;
+	}
+	
+	if (section->header.valid())
+		bringToFront(section->header.ptr());
+
+	if (section->footer.valid())
+		bringToFront(section->footer.ptr());
+	
+	_sections.push_back(section);
+	
+	adjustContentSize();
+	setOffsetDirectly(offset());
+	
+	return section;
+}
+
+void Table::setOffsetDirectly(const vec2& o)
+{
+	Scroll::setOffsetDirectly(o);
+	
+	float width = size().x;
+	float sectionStart = 0.0f;
+	float off = -offset().x;
+	for (auto s : _sections)
+	{
+		s->headerOffset = clamp(off - sectionStart, 0.0f, s->itemsSize);
+		s->footerOffset = clamp(width - s->sectionSize - sectionStart + off, -s->itemsSize, 0.0f);
+		sectionStart += s->sectionSize;
+	}
+	
+	layoutChildren(size());
+}
+
+void Table::clean()
+{
+	for (auto s : _sections)
+	{
+		s->header.reset(nullptr);
+		s->items.clear();
+		s->footer.reset(nullptr);
+		delete s;
+	}
+	
+	_sections.clear();
 }
